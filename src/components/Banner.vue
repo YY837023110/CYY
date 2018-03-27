70524a7901bc360e62da3eea7ce5156543689fd1
CYY <837023110@qq.com>
2018-03-27 21:05:27 +0800

    我的第一个

---
+++ src/components/Banner.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide" v-for="(item,index) in listImgs" :key="index">
+                <img :src="item.url" alt="">
+            </div>
+        </div>
+        <div class="swiper-pagination swiper-pagination-white"></div>
+    </div>
+</template>
+
+<script>
+    import Swiper from 'swiper';
+    import '../../static/css/swiper.min.css';
+    export default {
+        
+        props: ['listImgs'],
+        mounted() {
+            console.log('mounted', this);
+            console.log(this.listImgs)
+            var swiper = new Swiper('.swiper-container', {
+                pagination: '.swiper-pagination',
+                paginationClickable: true,
+                loop: true,
+                speed: 600,
+                autoplay: 4000,
+                onTouchEnd: function() {
+                    swiper.startAutoplay()
+                }
+            });
+        }
+    }
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <app-banner :listImgs="listImg"></app-banner>
+  </div>
+</template>
+
+<script>
+import Banner from "./Banner.vue";
+import a from "../../static/img/home/1.jpg";
+import b from "../../static/img/home/2.jpg";
+import c from "../../static/img/home/3.jpg";
+import d from "../../static/img/home/4.jpg";
+import e from "../../static/img/home/5.jpg";
+export default {
+  name: "Home",
+  mounted () {
+    console.log(this.listImg)
+  },
+  data() {
+    return {
+      listImg: [
+        {
+          url: a
+        },
+        {
+          url: b
+        },
+        {
+          url: c
+        },
+        {
+          url: d
+        },
+        {
+          url: e
+        }
+      ]
+    };
+  },
+  components: {
+    "app-banner": Banner
+  }
+};
+</script>
+
+<style>
+
+</style>
